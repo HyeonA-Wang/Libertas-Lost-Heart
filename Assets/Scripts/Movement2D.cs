92b433af729cdb7edce2ff90820ea679be781af1
arena17506310 <84490412+arena17506310@users.noreply.github.com>
2022-02-24 23:48:37 +0900

    대쉬 점프 리스폰 밀림방지

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
+    public float moveDefaulPower = 8f;
+    public float movePower = 0;
+    public float dashDelay = 10f;
+    public float dashTime = 0;
+    public float dashLimitTime = 0.1f;
+    public float dashPower = 40f;
+    public int jumpCount = 0;
+    public int jumpMax = 2;
 
-    public float movePower = 1f;
-    public float jumpPower = 1f;
+    public bool isRight = false;
+    public bool isLeft = false;
+    public bool isDash = false;
 
+
+    [SerializeField]
     Rigidbody2D rigid;
-
-    Vector3 movement;
-    bool isJumping = false;
-    // Start is called before the first frame update
 
     void Start()
     {
@@ -23,46 +30,63 @@ public class Movement2D : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown("left shift"))
         {
-            isJumping = true;
+            isDash = true;
         }
+        Debug.Log(isDash);
+
+
     }
-
-
     void FixedUpdate()
     {
         Move();
-        Jump();
+    }
 
+    void OnCollisionEnter2D(Collision2D other) //충돌했을 경우 호출
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) // 바닥과 충돌하면 i가 0이됨
+        {
+            jumpCount = jumpMax;
+        }
     }
 
     void Move()
 
     {
+        movePower = moveDefaulPower;
         Vector3 moveVelocity = Vector3.zero;
 
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
-
             moveVelocity = Vector3.left;
+            isRight = false;
+            isLeft = true;
         }
 
         else if (Input.GetAxisRaw("Horizontal") > 0)
         {
             moveVelocity = Vector3.right;
+            isLeft = false;
+            isRight = true;
         }
+
+        if (isDash == true)
+        {
+            movePower = dashPower;
+            dashTime += Time.deltaTime;
+            if (dashTime > dashLimitTime)
+            {
+                movePower = moveDefaulPower;
+                isDash = false;
+                dashTime = 0;
+            }
+        }
+
+
         transform.position += moveVelocity * movePower * Time.deltaTime;
-    }
-    void Jump()
-    {
-        if (!isJumping)
-            return;
-        rigid.velocity = Vector2.zero;
+        movePower = moveDefaulPower;
 
-        Vector2 jumpVelocity = new Vector2(0, jumpPower);
-        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
-
-        isJumping = false;
     }
+
 }
